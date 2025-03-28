4784042cfdfd17f6be999cc7cb660741c10b2870
CENTURION 1 <kdtkachenko@gmail.com>
2025-03-28 17:15:18 +0200

    Добавьте файлы проекта.

---
+++ Методы принятия решений/Form2.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Методы_принятия_решений
+{
+    partial class Form2 : Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtCriteria = new TextBox();
+            txtAlternatives = new TextBox();
+            btnGenerateTable = new Button();
+            dataGridView = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            dataGridViewComparison = new DataGridView();
+            label3 = new Label();
+            label4 = new Label();
+            dataGridView1 = new DataGridView();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewComparison).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtCriteria
+            // 
+            txtCriteria.Location = new Point(160, 91);
+            txtCriteria.Name = "txtCriteria";
+            txtCriteria.Size = new Size(125, 27);
+            txtCriteria.TabIndex = 0;
+            // 
+            // txtAlternatives
+            // 
+            txtAlternatives.Location = new Point(160, 157);
+            txtAlternatives.Name = "txtAlternatives";
+            txtAlternatives.Size = new Size(125, 27);
+            txtAlternatives.TabIndex = 1;
+            // 
+            // btnGenerateTable
+            // 
+            btnGenerateTable.Location = new Point(742, 285);
+            btnGenerateTable.Name = "btnGenerateTable";
+            btnGenerateTable.Size = new Size(94, 48);
+            btnGenerateTable.TabIndex = 2;
+            btnGenerateTable.Text = "Создать таблицы";
+            btnGenerateTable.UseVisualStyleBackColor = true;
+            btnGenerateTable.Click += btnGenerateTable_Click_1;
+            // 
+            // dataGridView
+            // 
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(352, 47);
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowHeadersWidth = 51;
+            dataGridView.Size = new Size(384, 231);
+            dataGridView.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 94);
+            label1.Name = "label1";
+            label1.Size = new Size(135, 20);
+            label1.TabIndex = 4;
+            label1.Text = "кол-во Критериев";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(0, 160);
+            label2.Name = "label2";
+            label2.Size = new Size(149, 20);
+            label2.TabIndex = 5;
+            label2.Text = "кол-во Альтернатив";
+            // 
+            // dataGridViewComparison
+            // 
+            dataGridViewComparison.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewComparison.Location = new Point(352, 335);
+            dataGridViewComparison.Name = "dataGridViewComparison";
+            dataGridViewComparison.RowHeadersWidth = 51;
+            dataGridViewComparison.Size = new Size(393, 243);
+            dataGridViewComparison.TabIndex = 6;
+            
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(388, 4);
+            label3.Name = "label3";
+            label3.Size = new Size(321, 40);
+            label3.TabIndex = 7;
+            label3.Text = "Значение критериев для альтернатив в виде\r\n              оценки по шкале Саате\r\n";
+            
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(388, 299);
+            label4.Name = "label4";
+            label4.Size = new Size(303, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Матрица попарных сравнений критериев";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(825, 47);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(389, 231);
+            dataGridView1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(858, 4);
+            label5.Name = "label5";
+            label5.Size = new Size(325, 40);
+            label5.TabIndex = 10;
+            label5.Text = "Значение критериев для альтернатив в виде \r\n                    словесного описания";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1254, 631);
+            Controls.Add(label5);
+            Controls.Add(dataGridView1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(dataGridViewComparison);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView);
+            Controls.Add(btnGenerateTable);
+            Controls.Add(txtAlternatives);
+            Controls.Add(txtCriteria);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewComparison).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtCriteria;
+        private TextBox txtAlternatives;
+        private Button btnGenerateTable;
+        private DataGridView dataGridView;
+        private Label label1;
+        private Label label2;
+        private DataGridView dataGridViewComparison;
+        private Label label3;
+        private Label label4;
+        private DataGridView dataGridView1;
+        private Label label5;
+    }
+}
---
+++ Методы принятия решений/Form2.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Методы_принятия_решений
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerateTable_Click_1(object sender, EventArgs e)
+        {
+            int criteriaCount, alternativesCount;
+
+            // Проверяем ввод
+            if (!int.TryParse(txtCriteria.Text, out criteriaCount) || criteriaCount < 2 || criteriaCount > 10)
+            {
+                MessageBox.Show("Введите количество критериев от 2 до 10.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(txtAlternatives.Text, out alternativesCount) || alternativesCount < 2 || alternativesCount > 10)
+            {
+                MessageBox.Show("Введите количество альтернатив от 2 до 10.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Создаем двумерный массив для хранения данных
+            string[,] tableData = new string[criteriaCount + 1, alternativesCount + 1];
+
+            // Запрашиваем названия альтернатив у пользователя
+            for (int i = 0; i < alternativesCount; i++)
+            {
+                tableData[0, i + 1] = Interaction.InputBox($"Введите название для альтернативы {i + 1}:", "Название альтернативы", $"Альтернатива {i + 1}");
+            }
+
+            // Запрашиваем названия критериев у пользователя
+            for (int i = 0; i < criteriaCount; i++)
+            {
+                tableData[i + 1, 0] = Interaction.InputBox($"Введите название для критерия {i + 1}:", "Название критерия", $"Критерий {i + 1}");
+            }
+
+            // Очищаем таблицы перед заполнением
+            dataGridView.Rows.Clear();
+            dataGridView.Columns.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            // Заполняем обе таблицы
+            FillDataGridView(dataGridView, tableData, alternativesCount, criteriaCount);
+            FillDataGridView(dataGridView1, tableData, alternativesCount, criteriaCount);
+
+            // Передаем критерии в GenerateComparisonMatrix
+            GenerateComparisonMatrix(tableData);
+        }
+
+
+        private void GenerateComparisonMatrix(string[,] tableData)
+        {
+            int criteriaCount = tableData.GetLength(0) - 1; // Количество критериев
+
+            // Очищаем таблицу сравнений
+            dataGridViewComparison.Rows.Clear();
+            dataGridViewComparison.Columns.Clear();
+
+            dataGridViewComparison.AllowUserToAddRows = false;
+
+            // Добавляем заголовки столбцов
+            dataGridViewComparison.Columns.Add("Criteria", ""); // Первый столбец с названиями критериев
+            for (int i = 1; i <= criteriaCount; i++)
+            {
+                dataGridViewComparison.Columns.Add($"C{i}", tableData[i, 0]); // Названия критериев
+            }
+
+            // Заполняем таблицу
+            for (int i = 1; i <= criteriaCount; i++)
+            {
+                object[] row = new object[criteriaCount + 1];
+                row[0] = tableData[i, 0]; // Название критерия в первом столбце
+
+                for (int j = 1; j <= criteriaCount; j++)
+                {
+                    if (i == j)
+                    {
+                        row[j] = 1; // Главная диагональ = 1
+                    }
+                    else
+                    {
+                        row[j] = ""; // Оставляем пустым для ввода пользователем
+                    }
+                }
+
+                dataGridViewComparison.Rows.Add(row);
+            }
+
+            // Добавляем обработчик изменения ячеек для симметричного заполнения
+            dataGridViewComparison.CellEndEdit += (sender, e) =>
+            {
+                int rowIdx = e.RowIndex;
+                int colIdx = e.ColumnIndex;
+
+                // Проверяем, что редактируется верхняя часть матрицы
+                if (rowIdx >= 0 && colIdx > 0 && rowIdx != colIdx)
+                {
+                    if (double.TryParse(dataGridViewComparison.Rows[rowIdx].Cells[colIdx].Value?.ToString(), out double value) && value > 0)
+                    {
+                        //НЕ ЗАБЫВАЕМ ЧТО ЗДЕСЬ СОЗДАЕМ СТРОКУ, А ЧИСЛОВОЕ ЗНАЧЕНИЕ !!!!
+                        dataGridViewComparison.Rows[colIdx - 1].Cells[rowIdx + 1].Value = $"1/{value}";
+                    }
+                    else
+                    {
+                        dataGridViewComparison.Rows[rowIdx].Cells[colIdx].Value = "";
+                        MessageBox.Show("Введите корректное положительное число!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            };
+        }
+
+
+        private void FillDataGridView(DataGridView grid, string[,] tableData, int alternativesCount, int criteriaCount)
+        {
+            // Добавляем столбцы
+            grid.Columns.Add("Criteria", ""); // Первый столбец для критериев
+            for (int j = 1; j <= alternativesCount; j++)
+            {
+                grid.Columns.Add($"Alt{j}", tableData[0, j]); // Названия альтернатив
+            }
+
+            // Заполняем строки
+            for (int i = 1; i <= criteriaCount; i++)
+            {
+                object[] row = new object[alternativesCount + 1];
+                row[0] = tableData[i, 0]; // Название критерия
+                grid.Rows.Add(row);
+            }
+        }  
+               
+    }
+}
